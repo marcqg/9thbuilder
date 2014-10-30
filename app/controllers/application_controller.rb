b7c22b2f85ed1aca3513e17b0ed617188b7af12d
Kevyn Lebouille <kevyn@opsone.net>
2014-10-30 21:02:47 +0100

    Default locale setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
   layout Proc.new { |controller| controller.request.xhr? ? false : 'application' }
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a? AdminUser
