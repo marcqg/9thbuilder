d7f3c8be7754cc1c7cc0027e0fbe2bdb0d406f0c
Kevyn Lebouille <kevyn@opsone.net>
2015-02-07 20:40:52 +0100

    Upgrade Rails 4.2.0

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Whbab
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :en
 
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
     # Local environment variables
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
