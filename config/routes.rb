46c7f096ef4daa4f81c8e60bc776fd5c66ce421d
Kevyn Lebouille <kevyn@opsone.net>
2015-12-20 22:47:59 +0100

    Exclude admin from routing-filter locale

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  filter :locale
+  filter :locale, exclude: /^\/admin/
 
   ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
