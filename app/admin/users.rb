cf29d5217b02de225a0017afce59e83456eac232
Marcqg <guillaume.marcq@vo2-consultants.com>
2017-03-29 17:32:05 +0200

    Modify user form with roles

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -4,6 +4,8 @@ ActiveAdmin.register User do
   filter :email
   filter :name
 
+  permit_params :email, :password, :password_confirmation, role_ids: []
+
   index do
     selectable_column
     id_column
@@ -14,4 +16,46 @@ ActiveAdmin.register User do
     column :updated_at
     actions
   end
+
+  form do |f|
+    f.inputs "User Details" do
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+      f.input :roles, as: :check_boxes
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :email
+      row :name
+      row :favorite_army, sortable: :favorite_army_id
+      row :created_at
+      row :updated_at
+      row :sign_in_count
+      row :current_sign_in_at
+      row :last_sign_in_at
+      row :current_sign_in_ip
+      row :last_sign_in_ip
+      row :confirmed_at
+      row :roles do |r|
+        r.roles.map { |role| role.name }.join(", ")
+      end
+    end
+  end
+
+  # Allow form to be submitted without a password
+  controller do
+    def update
+      if params[:user][:password].blank?
+        params[:user].delete "password"
+        params[:user].delete "password_confirmation"
+      end
+
+      super
+    end
+  end
+
 end
