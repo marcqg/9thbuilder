cde4e9e02d67f773af7d4db1370149e6d183e076
marcqg <marcqguillaume@gmail.com>
2017-04-08 22:58:15 +0200

    FK

--- db/migrate/20170405163108_add_organisation.rb
+++ db/migrate/20170405163108_add_organisation.rb
@@ -7,15 +7,8 @@ class AddOrganisation < ActiveRecord::Migration[5.0]
     end
     add_foreign_key :ninth_age_army_organisations, :armies, column: :army_id
 
-    reversible do |dir|
-      dir.up do
-        NinthAge::ArmyOrganisation.create_translation_table! :name => :string, :description => :string
-      end
-
-      dir.down do
-        NinthAge::ArmyOrganisation.drop_translation_table!
-      end
-    end
+    NinthAge::ArmyOrganisation.create_translation_table! :name => :string, :description => :string
+    add_foreign_key :ninth_age_army_organisation_translations, :ninth_age_army_organisations, column: :ninth_age_army_organisation_id, on_delete: :cascade
 
     create_table :ninth_age_organisations do |t|
       t.belongs_to :army, index: false, null: false, default: 0
@@ -24,15 +17,8 @@ class AddOrganisation < ActiveRecord::Migration[5.0]
     end
     add_foreign_key :ninth_age_organisations, :armies, column: :army_id
 
-    reversible do |dir|
-      dir.up do
-        NinthAge::Organisation.create_translation_table! :name => :string
-      end
-
-      dir.down do
-        NinthAge::Organisation.drop_translation_table!
-      end
-    end
+    NinthAge::Organisation.create_translation_table! :name => :string
+    add_foreign_key :ninth_age_organisation_translations, :ninth_age_organisations, column: :ninth_age_organisation_id, on_delete: :cascade
 
     create_table :ninth_age_organisation_groups do |t|
       t.belongs_to :army_organisation, null: false, default: 0
@@ -47,12 +33,15 @@ class AddOrganisation < ActiveRecord::Migration[5.0]
     create_table :ninth_age_organisation_changes do |t|
       t.belongs_to :default_organisation, index: true, null: false, default: 0,  :references => [:organisations, :id]
       t.belongs_to :new_organisation, index: true, null: false, default: 0, :references => [:organisations, :id]
-      t.belongs_to :figurine, index: true, null: false, default: 0
+      t.belongs_to :unit, index: true, null: false, default: 0
       t.integer :number, null: false, default: 0
       t.column :type_target, :integer, default: 0
 
       t.timestamps
     end
+    add_foreign_key :ninth_age_organisation_changes, :ninth_age_organisations, column: :default_organisation_id
+    add_foreign_key :ninth_age_organisation_changes, :ninth_age_organisations, column: :new_organisation_id
+    add_foreign_key :ninth_age_organisation_changes, :units, column: :unit_id
 
   end
 end
