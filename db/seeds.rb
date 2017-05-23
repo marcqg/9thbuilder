2ca8ce940bd0c7e0fd71f84e33e659544e1900ea
Marcqg <guillaume.marcq@vo2-consultants.com>
2017-05-23 13:27:32 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-AdminUser.create email: 'admin@example.com', password: 'password', password_confirmation: 'password'
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
