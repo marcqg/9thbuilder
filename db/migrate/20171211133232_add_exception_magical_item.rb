3372c02c3ab5a705267ebda6c8ff6fe21c778998
Marcqg <guillaume.marcq@vo2-consultants.com>
2017-12-11 16:41:02 +0100

    Seulement les runes pour les nains

--- db/migrate/20171211133232_add_exception_magical_item.rb
+++ db/migrate/20171211133232_add_exception_magical_item.rb
@@ -2,6 +2,6 @@ class AddExceptionMagicalItem < ActiveRecord::Migration[5.0]
   def change
   	add_column :ninth_age_armies, :has_default_magic_items, :boolean, :null => false, :default => true
 
-  	NinthAge::Army.where(:id => [3, 18, 34]).update_all(:has_default_magic_items => false)
+  	NinthAge::Army.where(:id => [3, 18, 34, 5, 20, 36]).update_all(:has_default_magic_items => false)
   end
 end
