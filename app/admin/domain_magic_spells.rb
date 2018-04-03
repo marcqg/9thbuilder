304fb5b334b2ba6e56a724730fd3644f492df7db
Guillaume Marcq <marcqguillaume@gmail.com>
2018-04-03 14:26:28 +0200

    type lvl pour un spell

--- app/admin/domain_magic_spells.rb
+++ app/admin/domain_magic_spells.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register NinthAge::DomainMagicSpell do
   menu parent: 'Ninth Age Magic', priority: 2
 
-  permit_params :domain_magic_id, {:type_target => []}, {:duration => []}, :locale, translations_attributes: [:id, :name, :range, :casting_value, :effect, :locale, :_destroy]
+  permit_params :domain_magic_id, :type_lvl, {:type_target => []}, {:duration => []}, :locale, translations_attributes: [:id, :name, :range, :casting_value, :effect, :locale, :_destroy]
 
   filter :domain_magic, as: :select, collection: -> { NinthAge::DomainMagic.includes(:translations).map { |version| [ version.name, version.id ] } } 
   
@@ -26,6 +26,7 @@ ActiveAdmin.register NinthAge::DomainMagicSpell do
   index do
     selectable_column
     id_column
+    column :type_lvl
     column :name
     column :domain_magic, sortable: :domain_magic_id
     actions
@@ -33,9 +34,10 @@ ActiveAdmin.register NinthAge::DomainMagicSpell do
 
   form do |f|
     f.inputs do
-      f.input :domain_magic, collection: NinthAge::DomainMagic.includes(:translations).includes(:version).order(:name).collect { |o| [o.name + ' - ' + o.version.name, o.id] }, :prompt => true
+      f.input :domain_magic, collection: NinthAge::DomainMagic.includes(:translations).includes(:version => [:translations]).order(:name).collect { |o| [o.name + ' - ' + o.version.name, o.id] }, :prompt => true
       f.input :type_target, as: :check_boxes, collection: NinthAge::DomainMagicSpell.values_for_type_target.collect { |type_target| [I18n.t("magic_spell.type_target.#{type_target}", default: type_target), type_target] }
       f.input :duration, as: :check_boxes, collection: NinthAge::DomainMagicSpell.values_for_duration.collect { |duration| [I18n.t("magic_spell.duration.#{duration}", default: duration), duration] }
+      f.input :type_lvl, :as => :select, :collection =>  NinthAge::DomainMagicSpell.type_lvls, :include_blank => false
       f.translate_inputs do |t|
         t.input :name
         t.input :range
@@ -53,6 +55,7 @@ ActiveAdmin.register NinthAge::DomainMagicSpell do
       row :domain_magic
       row :type_target
       row :duration
+      row :type_lvl
       row :latex_key
     end
     panel 'Translations' do
