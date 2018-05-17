016c553a3b45d03e9c2089e154286b78a6a21d54
Guillaume Marcq <marcqguillaume@gmail.com>
2018-05-17 15:51:43 +0200

    Amélioration création Tournoi

--- app/controllers/tournament/rounds_controller.rb
+++ app/controllers/tournament/rounds_controller.rb
@@ -7,25 +7,54 @@ module Tournament
     # GET /tournament/event-{event_id}/rounds/{id}
     def show
       if @round.matches.count < (@event.user_applies.count / 2)
+        p "create match"
 
-        user_in_match_ids = @round.matches.map(&:user_apply_id)
+        # All users already in this round
+        user_in_match_ids = @round.matches
+                                  .where(:round_id => @round.id)
+                                  .map(&:user_apply_id)
+        p "user_in_match_ids"
+        p user_in_match_ids
 
         matches = @event.matches
-                    .where.not(user_apply_id: user_in_match_ids)
-                    .group_by  {|h| h.user_apply_id}
-                    .map{|k,v| [k,v.map{ |h| h.points }.sum] }
-                    .sort_by {|k| k[1]}
-                    .map{ |k| k[0]}
-                    .reverse
-                    .in_groups_of(2)
-                    .select { |hash| !hash[0].nil? && !hash[1].nil? }
+                        .where.not(:round_id => @round.id)
+                        .where.not(:user_apply_id => user_in_match_ids)
+                        .group_by  {|h| h.user_apply_id}
+                        .map{|k,v| [k,v.map{ |m| m.points }.sum] }
+        p "matches"
+        p matches
 
-        matches.each_with_index do |group,index|
-          group.each do |user|
-            Tournament::Match.create!({ 
-              :round_id => @round.id, 
-              :table => index + 1,
-              :user_apply_id => user })
+        # All user applies not in this round
+        users = @event.user_applies
+                              .where.not(id: user_in_match_ids)
+                              .map(&:id)
+        p "users"
+        p users
+
+        users.each do |user|
+          matches << [user, 0]
+        end
+        p "matches"
+        p matches
+
+        user_in_matches = matches.group_by  {|h| h[0]}
+                                  .map{|k,v| [k,v.map{ |m| m[1] }.sum] }
+                                  .sort{|x,y| y[1] <=> x[1]}
+                                  .map{ |k| k[0]}
+
+        p "user_in_matches"
+        p user_in_matches
+
+        table = @round.matches.count / 2 + 1
+
+        user_in_matches.each_with_index do |user, index|
+          Tournament::Match.create!({ 
+            :round_id => @round.id, 
+            :table => table,
+            :user_apply_id => user })
+
+          if (index % 2 == 1)
+            table = table + 1
           end
         end
 
