587ccffd844acb4882d1436289981bfacfa33b0b
Guillaume Marcq <marcqguillaume@gmail.com>
2018-05-28 12:13:48 +0200

    Amylist json format

--- app/controllers/api/v1/builder/army_lists_controller.rb
+++ app/controllers/api/v1/builder/army_lists_controller.rb
@@ -11,8 +11,11 @@ module Api
         def index
 
           @army_lists = ::Builder::ArmyList.where(user: current_user)
-                            .includes(:army)
                             .order('value_points DESC')
+
+          respond_to do |format|
+            format.json
+          end
         end
       end
     end
