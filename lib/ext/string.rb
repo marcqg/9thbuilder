f661ec47cf1de1f510825cdab152c24b47f35641
Guillaume Marcq <marcqguillaume@gmail.com>
2018-06-14 11:54:20 +0200

    Fix bb-code parser

--- lib/ext/string.rb
+++ lib/ext/string.rb
@@ -1,8 +1,17 @@
 class String
   def to_highlight
-    text = self.bb_to_html
-        .gsub(/(?:\n\r?|\r\n?)/, '<br/>')
-        .html_safe
+    text = self
+    if text.nil? or text.empty?
+      return text
+    end
+
+    text = RuBB.to_html(text)
+
+    unless text.nil?
+      text = text.gsub(/(?:\n\r?|\r\n?)/, '<br/>')
+                 .html_safe
+    end
+
     text
   end
 end
