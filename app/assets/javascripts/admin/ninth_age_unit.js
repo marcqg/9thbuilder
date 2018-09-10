a28bb68687c063200151ddef40df28eafb74e7f3
Guillaume Marcq <marcqguillaume@gmail.com>
2018-09-10 18:04:43 +0200

    Fix affichage carac unit V2

--- app/assets/javascripts/admin/ninth_age_unit.js
+++ app/assets/javascripts/admin/ninth_age_unit.js
@@ -1,5 +1,20 @@
 jQuery(function($) {
 
+    //Create NinthAge::Unit
+    $('#new_ninth_age_unit #ninth_age_unit_type_carac').change(function() {
+        var self = $(this);
+
+        self.find("option").each(function() {
+            if ($(this).val() != self.val()) {
+                $('#ninth_age_unit_carac_' + $(this).val().toLowerCase()).hide();
+            }
+        });
+
+        $('#ninth_age_unit_carac_' + self.val().toLowerCase()).show();
+
+    });
+    $('#new_ninth_age_unit #ninth_age_unit_type_carac').change();
+
     //Edit NinthAge::Unit
     $('#edit_ninth_age_unit #ninth_age_unit_type_carac').change(function() {
         var self = $(this);
